2eda2c97d316bff662e01c9e43c13c32bf226c00
João Pedro Odorizzio de Souza <joao.310502@alunos.utfpr.edu.br>
2023-10-09 11:28:12 -0300

    atualizando o aprendizado de js

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log('a velocidade do seu carro é ' + vel + 'Km/h')
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} // Condição simples!
+console.log('Dirija sempre usando cinto de segurança!')
+
